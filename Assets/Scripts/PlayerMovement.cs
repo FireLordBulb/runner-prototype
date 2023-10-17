9f5f0dd023bc8061e3a84d50697cd3ec96685a80
Lserafindev <lserafin1748@gmail.com>
2023-10-17 14:39:27 +0200

    added basic movement functionality

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -1,18 +1,36 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.PlasticSCM.Editor.WebApi;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PlayerMovement : MonoBehaviour
 {
-    // Start is called before the first frame update
+    [SerializeField] private int lanes;
+    [SerializeField] private float laneDistance;
+    //[SerializeField] private float lerpSpeed;
+    [SerializeField] private InputActionReference MoveLeft, MoveRight;
+
+    private int currentLane = 0;
+
     void Start()
     {
-        
+        currentLane = (int)Mathf.Floor(lanes * 0.5f);
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+
+        if (MoveLeft.action.WasPressedThisFrame())
+        {
+            currentLane -= 1;
+        }
+
+        if (MoveRight.action.WasPressedThisFrame())
+        {
+            currentLane += 1;
+        }
+        currentLane = (int)Mathf.Clamp(currentLane, 0, lanes - 1);
+        transform.position = new Vector3((currentLane - Mathf.Floor(lanes * 0.5f))*laneDistance,0,0);
     }
 }

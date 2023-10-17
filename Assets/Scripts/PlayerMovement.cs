24495ba84205390c9b89821f6e05b3b9761a20e2
Lserafindev <lserafin1748@gmail.com>
2023-10-17 15:02:24 +0200

    made the movement more better and less basic

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using Unity.PlasticSCM.Editor.WebApi;
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
@@ -8,10 +9,12 @@ public class PlayerMovement : MonoBehaviour
 {
     [SerializeField] private int lanes;
     [SerializeField] private float laneDistance;
-    //[SerializeField] private float lerpSpeed;
+    [SerializeField] private float lerpSpeed;
     [SerializeField] private InputActionReference MoveLeft, MoveRight;
 
     private int currentLane = 0;
+    private int previousLane = 0;
+    private float currentLerp = 0;
 
     void Start()
     {
@@ -20,17 +23,34 @@ public class PlayerMovement : MonoBehaviour
 
     void Update()
     {
-
-        if (MoveLeft.action.WasPressedThisFrame())
+        if (currentLane == previousLane)
         {
-            currentLane -= 1;
+
+            if (MoveLeft.action.WasPressedThisFrame())
+            {
+                previousLane = currentLane;
+                currentLane -= 1;
+            }
+
+            if (MoveRight.action.WasPressedThisFrame())
+            {
+                previousLane = currentLane;
+                currentLane += 1;
+            }
         }
 
-        if (MoveRight.action.WasPressedThisFrame())
-        {
-            currentLane += 1;
-        }
         currentLane = (int)Mathf.Clamp(currentLane, 0, lanes - 1);
-        transform.position = new Vector3((currentLane - Mathf.Floor(lanes * 0.5f))*laneDistance,0,0);
+        currentLerp = (currentLane == previousLane) ? 0 : currentLerp + lerpSpeed * Time.deltaTime;
+        
+        if (currentLerp > 0.99f) {
+            previousLane = currentLane;
+            currentLerp = 0;
+        }
+
+        var posOld = (previousLane - Mathf.Floor(lanes * 0.5f)) * laneDistance;
+        var posNew = (currentLane - Mathf.Floor(lanes * 0.5f)) * laneDistance;
+
+        transform.position = new Vector3(Mathf.Lerp(posOld,posNew,currentLerp),0,0);
+        
     }
 }
